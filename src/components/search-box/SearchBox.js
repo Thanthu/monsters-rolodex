80d7a585b1ec8a8c7d26b5e0b606e5e0ae2f85de
Thanthu <tnair@orthofx.com>
2022-04-12 21:24:48 +0530

    refactor SearchBox component to use functional component instead of class component

--- src/components/search-box/SearchBox.js
+++ src/components/search-box/SearchBox.js
@@ -1,12 +1,8 @@
-import { Component } from "react";
 import './SearchBox.css'
 
-class SearchBox extends Component {
-
-    render() {
-        const { className, placeholder, onChangeHandler } = this.props;
-        return <input className={className} type='search' placeholder={placeholder} onChange={onChangeHandler} />
-    }
+function SearchBox(props) {
+    const { className, placeholder, onChangeHandler } = props;
+    return <input className={className} type='search' placeholder={placeholder} onChange={onChangeHandler} />
 }
 
 export default SearchBox;
